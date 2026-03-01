5710e4f98995c7372a8fea6a025f1c67096ae7a2
i4nizer <iansandoval264@gmail.com>
2026-03-01 10:26:54 +0800

    Created coffee leaf detection worker.

---
+++ src/tasks/cld.detection.task.ts
@@ -0,0 +1,26 @@
+import yoloPipelineMulticlass from "@/utils/yolo.pipeline.multiclass";
+
+//
+
+const { VITE_AI_CLD_URL, VITE_AI_CLD_IMGSZ, VITE_AI_CLD_CLASSES } = import.meta.env
+const [url, imgsz, classes] = [VITE_AI_CLD_URL, Number(VITE_AI_CLD_IMGSZ), VITE_AI_CLD_CLASSES?.split(", ")]
+
+self.postMessage("AI model is loading please wait.")
+const model = await yoloPipelineMulticlass.load(url)
+await yoloPipelineMulticlass.warmup(model, imgsz)
+self.postMessage("AI model loaded successfully.")
+
+const backend = await yoloPipelineMulticlass.backend()
+const isGpu = backend.toLowerCase().includes("webgl")
+if (isGpu) self.postMessage(`AI model is gpu accelerated, uses ${backend}.`)
+else self.postMessage(`AI model is not gpu accelerated, uses ${backend}.`)
+
+//
+
+self.onmessage = async (e: MessageEvent<ImageBitmap>) => {
+    const alpha = performance.now()
+    const bboxes = await yoloPipelineMulticlass.predict(model, e.data, imgsz, classes, 0.9, 0.25, 100)
+    self.postMessage(bboxes)
+}
+
+//
