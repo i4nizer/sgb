07dfb25bc6e9559dfca1c6e3f7a91c05f2497a60
i4nizer <iansandoval264@gmail.com>
2026-03-08 13:09:07 +0800

    Removed redirect on api util to avoid loop.

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,22 +1,8 @@
-import type { AxiosInterceptorRejected } from "node_modules/axios/index.d.cts";
-import router from "../router";
 import axios from "axios";
-import useToast from "@/composables/use-toast";
 
 //
 
 const api = axios.create({ baseURL: import.meta.env.VITE_API_URL, withCredentials: true })
-
-//
-
-const onRejected: AxiosInterceptorRejected = async (err) => {
-    if (err?.response?.status != 401) return Promise.reject(err)
-    const toastCmp = useToast()
-    toastCmp.error("Session expired, kindly login again.")
-    return await router.push("/auth/sign-in")
-}
-
-api.interceptors.response.use((res) => res, onRejected)
 
 //
 
