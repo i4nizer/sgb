b1cd8daf70358fbfd97cddf2956a5e08013caebd
i4nizer <iansandoval264@gmail.com>
2026-03-08 22:53:05 +0800

    Implemented reading fetch in monitor page.

--- src/views/app/MonitorView.vue
+++ src/views/app/MonitorView.vue
@@ -4,6 +4,25 @@
             <v-col cols="12" lg="6">
                 <v-card class="pt-4" elevation="1">
                     <template #prepend>
+                        <v-icon color="blue">mdi-water</v-icon>
+                    </template>
+                    <template #title>
+                        <span>Humidity</span>
+                    </template>
+                    <template #subtitle>
+                        <span>24-hour monitoring</span>
+                    </template>
+                    <template #text>
+                        <ReadingChart 
+                            :color="themeCmp.current.value.colors.accent"
+                            :readings="humidities"
+                        ></ReadingChart>
+                    </template>
+                </v-card>
+            </v-col>
+            <v-col cols="12" lg="6">
+                <v-card class="pt-4" elevation="1">
+                    <template #prepend>
                         <v-icon color="red">mdi-thermometer</v-icon>
                     </template>
                     <template #title>
@@ -13,7 +32,10 @@
                         <span>24-hour monitoring</span>
                     </template>
                     <template #text>
-                        <ReadingChart :readings="readingsDHT"></ReadingChart>
+                        <ReadingChart 
+                            :color="themeCmp.current.value.colors.accent"
+                            :readings="temperatures"
+                        ></ReadingChart>
                     </template>
                 </v-card>
             </v-col>
@@ -29,7 +51,10 @@
                         <span>24-hour monitoring</span>
                     </template>
                     <template #text>
-                        <ReadingChart :readings="readingsSoilMoisture"></ReadingChart>
+                        <ReadingChart 
+                            :color="themeCmp.current.value.colors.accent"
+                            :readings="soilMoistures"
+                        ></ReadingChart>
                     </template>
                 </v-card>
             </v-col>
@@ -39,30 +64,30 @@
 
 <script setup lang="ts">
 import ReadingChart from '@/components/app/monitor/ReadingChart.vue';
-import type { ReadingChartSchema } from '@/schemas/ReadingSchema';
+import useToast from '@/composables/use-toast';
+import { useTheme } from 'vuetify';
+import { useReadingStore } from '@/stores/reading';
+import { onMounted, toRefs } from 'vue';
 
 //
 
-const readingsDHT: ReadingChartSchema[] = [
-    { name: "Humidity", unit: "%", value: 87.1, createdAt: new Date(Date.now() - 4 * 5 * 60 * 60 * 1000) },
-    { name: "Humidity", unit: "%", value: 84.9, createdAt: new Date(Date.now() - 4 * 4 * 60 * 60 * 1000) },
-    { name: "Humidity", unit: "%", value: 80.8, createdAt: new Date(Date.now() - 4 * 3 * 60 * 60 * 1000) },
-    { name: "Humidity", unit: "%", value: 78.7, createdAt: new Date(Date.now() - 4 * 2 * 60 * 60 * 1000) },
-    { name: "Humidity", unit: "%", value: 73.4, createdAt: new Date(Date.now() - 4 * 1 * 60 * 60 * 1000) },
-    { name: "Temperature", unit: "C", value: 27.1, createdAt: new Date(Date.now() - 4 * 5 * 60 * 60 * 1000) },
-    { name: "Temperature", unit: "C", value: 29.9, createdAt: new Date(Date.now() - 4 * 4 * 60 * 60 * 1000) },
-    { name: "Temperature", unit: "C", value: 30.8, createdAt: new Date(Date.now() - 4 * 3 * 60 * 60 * 1000) },
-    { name: "Temperature", unit: "C", value: 31.7, createdAt: new Date(Date.now() - 4 * 2 * 60 * 60 * 1000) },
-    { name: "Temperature", unit: "C", value: 30.4, createdAt: new Date(Date.now() - 4 * 1 * 60 * 60 * 1000) },
-]
+// --- Utils
+const toastCmp = useToast()
+const themeCmp = useTheme()
 
-const readingsSoilMoisture: ReadingChartSchema[] = [
-    { name: "Soil Moisture", unit: "%", value: 87.1, createdAt: new Date(Date.now() - 4 * 5 * 60 * 60 * 1000) },
-    { name: "Soil Moisture", unit: "%", value: 78.9, createdAt: new Date(Date.now() - 4 * 4 * 60 * 60 * 1000) },
-    { name: "Soil Moisture", unit: "%", value: 69.8, createdAt: new Date(Date.now() - 4 * 3 * 60 * 60 * 1000) },
-    { name: "Soil Moisture", unit: "%", value: 59.7, createdAt: new Date(Date.now() - 4 * 2 * 60 * 60 * 1000) },
-    { name: "Soil Moisture", unit: "%", value: 45.4, createdAt: new Date(Date.now() - 4 * 1 * 60 * 60 * 1000) },
-]
+// --- Reading
+const readingStore = useReadingStore()
+const { humidities, temperatures, soilMoistures } = toRefs(readingStore)
+
+//
+
+const onMountedCb = async () => {
+    await readingStore
+        .getReadings()
+        .catch(() => toastCmp.error("Something went wrong."))
+}
+
+onMounted(onMountedCb)
 
 //
 
