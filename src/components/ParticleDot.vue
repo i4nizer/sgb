1734a598ef4eee033f3726ea8ac90d023acb11d6
i4nizer <iansandoval264@gmail.com>
2026-03-09 01:59:12 +0800

    Created particle dot.

---
+++ src/components/ParticleDot.vue
@@ -0,0 +1,31 @@
+<template>
+	<motion.div 
+		:key="id"
+		:class="`position-absolute rounded-circle bg-${color}`"
+		:style="{ width: `4px`, height: `4px`, left: `${x}vw`, top: `${y}vh` }"
+		:animate="{ y: [0, -20, 0], opacity: [0.8, 1, 0.8] }"
+		:transition="{ duration: duration, repeat: Infinity, delay: delay }"
+	></motion.div>
+</template>
+
+<script setup lang="ts">
+import { motion } from "motion-v"
+
+//
+
+const props = defineProps<{
+    id: number,
+    x: number,
+    y: number,
+    color: string,
+    delay: number,
+    duration: number,
+}>()
+
+//
+
+</script>
+
+<style scoped>
+
+</style>
