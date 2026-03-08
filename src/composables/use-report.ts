1369536da96c4f4dc2364a190e04dd0957a2781a
i4nizer <iansandoval264@gmail.com>
2026-03-08 23:30:49 +0800

    Created use report composable that can generate pdf.

---
+++ src/composables/use-report.ts
@@ -0,0 +1,57 @@
+import jsPDF from 'jspdf'
+import html2canvas from 'html2canvas'
+
+//
+
+export default () => {
+
+    //
+
+    const generatePDF = async (el: HTMLElement) => {
+        // --- Create A4 report
+        const pdf = new jsPDF({
+            orientation: "p",
+            unit: "px",
+            format: "a4",
+            hotfixes: ["px_scaling"],
+        })
+
+        // --- 96px = 1inch
+        const pdfWidth = pdf.internal.pageSize.getWidth()
+        const pdfHeight = pdf.internal.pageSize.getHeight()
+
+        // --- Create canvas of the element
+        const canvas = await html2canvas(el, {
+            useCORS: true,
+            width: el.scrollWidth,
+            height: el.scrollHeight,
+        })
+
+        // --- Convert canvas to image in ratio
+        const imgData = canvas.toDataURL("image/png")
+        const imgWidth = pdfWidth
+        const imgHeight = (canvas.height * imgWidth) / canvas.width
+
+        // --- Record increments
+        let heightLeft = imgHeight
+        let heightUsed = 0
+
+        // --- Slice the first part of the image
+        pdf.addImage(imgData, "PNG", 0, heightUsed, imgWidth, imgHeight)
+        heightLeft -= pdfHeight
+
+        // --- Pull up the image
+        while (heightLeft > 0) {
+            heightUsed -= pdfHeight
+            pdf.addPage()
+            pdf.addImage(imgData, "PNG", 0, heightUsed, imgWidth, imgHeight)
+            heightLeft -= pdfHeight
+        }
+
+        return pdf
+    }
+
+    //
+
+    return { generatePDF }
+}
