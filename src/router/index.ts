cd9553cfd9d1481907d9a752c91e90e57e6d48fb
i4nizer <iansandoval264@gmail.com>
2026-03-08 13:10:11 +0800

    Implemented auth navigation guards.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory, type RouteRecordRaw } from "vue-router"
+import { redirectAuth, refreshAuth, requireAuth } from "@/middlewares/auth.middleware"
 import SignInView from "@/views/auth/SignInView.vue"
 import HomeView from "@/views/app/HomeView.vue"
 import MonitorView from "@/views/app/MonitorView.vue"
 import GrowthView from "@/views/app/GrowthView.vue"
 import SettingsView from "@/views/app/SettingsView.vue"
+import WelcomeView from "@/views/WelcomeView.vue"
 
 //
 
@@ -11,38 +13,44 @@ const routes: RouteRecordRaw[] = [
     {
         path: "/",
         name: "welcome",
-        meta: { layout: "app" },
-        redirect: "/auth/sign-in",
+        meta: { layout: "home" },
+        component: WelcomeView,
+        beforeEnter: [refreshAuth, redirectAuth, requireAuth],
     },
     {
         path: "/auth/sign-in",
         name: "sign-in",
         meta: { layout: "auth" },
         component: SignInView,
+        beforeEnter: [refreshAuth, redirectAuth],
     },
     {
         path: "/app/home",
         name: "home",
         meta: { layout: "app" },
         component: HomeView,
+        beforeEnter: [refreshAuth, requireAuth],
     },
     {
         path: "/app/monitor",
         name: "monitor",
         meta: { layout: "app" },
         component: MonitorView,
+        beforeEnter: [refreshAuth, requireAuth],
     },
     {
         path: "/app/growth",
         name: "growth",
         meta: { layout: "app" },
         component: GrowthView,
+        beforeEnter: [refreshAuth, requireAuth],
     },
     {
         path: "/app/settings",
         name: "settings",
         meta: { layout: "app" },
         component: SettingsView,
+        beforeEnter: [refreshAuth, requireAuth],
     },
 ]
 
