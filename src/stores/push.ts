a89ca0463600dad0f1e0e0d4c4dbfa81958a489a
i4nizer <iansandoval264@gmail.com>
2026-03-10 00:42:55 +0800

    Added push notif store.

---
+++ src/stores/push.ts
@@ -0,0 +1,35 @@
+import api from "@/utils/api"
+import { PushNotifications, type Token } from "@capacitor/push-notifications"
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+//
+
+export const usePushStore = defineStore("push", () => {
+    
+    //
+
+    const connected = ref(false)
+
+    //
+
+    const connect = async () => {
+        if (connected.value) return
+        const perm = await PushNotifications.requestPermissions()
+        if (perm.receive !== "granted") return console.info(`Push notification permission denied.`)
+    
+        await PushNotifications.register()
+        PushNotifications.addListener("registration", register)
+    }
+
+    const register = async (token: Token) => {
+        await api
+            .post("/device", { token: token.value })
+            .then(() => connected.value = true)
+            .catch(() => connected.value = false)
+    }
+
+    //
+
+    return { connected, connect, register }
+})
