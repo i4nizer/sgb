21a3be9fb7835341a314512e99219152c10224f2
i4nizer <iansandoval264@gmail.com>
2026-03-13 23:41:57 +0800

    Added reading websocket connection on main app.

--- src/App.vue
+++ src/App.vue
@@ -34,6 +34,9 @@ import { useAuthStore } from './stores/auth'
 import { usePushStore } from './stores/push'
 import { onMounted, ref, type Component } from 'vue'
 import { useRouter } from 'vue-router'
+import useWebsocket from './composables/use-websocket'
+import { useReadingStore } from './stores/reading'
+import type { ReadingSchema } from './schemas/ReadingSchema'
 
 //
 
@@ -56,20 +59,36 @@ const layouts: Record<string, Component> = {
 const routerCmp = useRouter()
 const isRouting = ref(false)
 
+// --- Reading
+const websocketCmp = useWebsocket()
+const readingStore = useReadingStore()
+
+const onWsReading = (data: ReadingSchema[]) => {
+	readingStore.readings.push(...data)
+}
+
 //
 
 const onMountedCb = async () => {
+	// --- Theme
 	const savedTheme = localStorage.getItem("theme") ?? "system"
 	themeCmp.change(savedTheme)
 
+	// --- Reading
+	websocketCmp.connect(`${import.meta.env.VITE_API_URL}/ws/app`)
+	websocketCmp.subscribe("Reading", "Create", onWsReading)
+
+	// --- Status Bar
 	const native = Capacitor.isNativePlatform()
 	if (native) StatusBar.setBackgroundColor({ color: "#00000000" })
 	if (native) StatusBar.setOverlaysWebView({ overlay: true })
 	if (native) StatusBar.setStyle({ style: savedTheme == "dark" ? Style.Dark : Style.Light })
 
+	// --- Route Loader
 	routerCmp.beforeEach(() => isRouting.value = true)
 	routerCmp.afterEach(() => isRouting.value = false)
-	
+
+	// --- Push Notifications
 	if (authStore.user) await pushStore.connect()
 }
 
