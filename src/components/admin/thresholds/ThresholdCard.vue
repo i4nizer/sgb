67605836e9ded872f2d5062e7438bf92806511c0
i4nizer <iansandoval264@gmail.com>
2026-03-22 01:21:21 +0800

    Added delete to threshold card.

--- src/components/admin/thresholds/ThresholdCard.vue
+++ src/components/admin/thresholds/ThresholdCard.vue
@@ -9,12 +9,18 @@
         <template #text>
             <div class="d-flex flex-column align-start ga-2">
                 <h3 class="pl-2">{{ threshold.operator }} {{ threshold.value }}</h3>
-                <div class="w-100 d-flex align-center justify-space-between">
+                <div class="w-100 d-flex align-center ga-2">
                     <span class="text-caption text-grey">"{{ threshold.message }}"</span>
+                    <v-spacer></v-spacer>
                     <v-btn 
                         size="x-small" 
                         icon="mdi-pencil-outline text-blue"
                         @click="onClickEdit"
+                    ></v-btn>
+                    <v-btn 
+                        size="x-small" 
+                        icon="mdi-delete-outline text-red"
+                        @click="onClickDelete"
                     ></v-btn>
                 </div>
             </div>
@@ -30,6 +36,7 @@ import type { ThresholdSchema } from '@/schemas/ThresholdSchema';
 const props = defineProps<{
     threshold: ThresholdSchema,
     onEdit?: (data: ThresholdSchema) => any
+    onDelete?: (data: ThresholdSchema) => any
     onError?: (error: any) => any
 }>()
 
@@ -39,6 +46,13 @@ const onClickEdit = async () => {
     await Promise
         .resolve()
 		.then(() => props.onEdit && props.onEdit(props.threshold))
+		.catch(err => props.onError && props.onError(err))
+}
+
+const onClickDelete = async () => {
+    await Promise
+        .resolve()
+		.then(() => props.onDelete && props.onDelete(props.threshold))
 		.catch(err => props.onError && props.onError(err))
 }
 
